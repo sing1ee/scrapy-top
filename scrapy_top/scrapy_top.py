3c9d790ba33734d8c01778c7361d68e0c875f983
zhangcheng <zhangcheng@zhangchengdeMacBook-Pro.local>
2014-04-01 11:14:41 +0800

    main

--- scrapy_top/scrapy_top.py
+++ scrapy_top/scrapy_top.py
@@ -20,28 +20,30 @@ for op, value in opts:
     elif op == "-p":
         PORT == int(value)
 
-i = os.system("clear")
-
-try:
-    tn = telnetlib.Telnet()
-    tn.open(HOST, PORT)
-except:
-    print "Can't connect to %s:%d" % (HOST, PORT)
-    sys.exit()
-
-while True:
+def main():
+    i = os.system("clear")
     try:
         tn = telnetlib.Telnet()
         tn.open(HOST, PORT)
-        tn.write('stats.get_stats()' + '\r\n')
-        ret = tn.read_until('}').splitlines()[3]
-        data = eval(ret)
-        x = PrettyTable(['Key', 'Value'])
-        for k, v in data.items():
-            x.add_row([k, v])
-            print x
-            time.sleep(2)
-            i = os.system("clear")
     except:
         print "Can't connect to %s:%d" % (HOST, PORT)
         sys.exit()
+        while True:
+            try:
+                tn = telnetlib.Telnet()
+                tn.open(HOST, PORT)
+                tn.write('stats.get_stats()' + '\r\n')
+                ret = tn.read_until('}').splitlines()[3]
+                data = eval(ret)
+                x = PrettyTable(['Key', 'Value'])
+                for k, v in data.items():
+                    x.add_row([k, v])
+                    print x
+                    time.sleep(2)
+                    i = os.system("clear")
+            except:
+                print "Can't connect to %s:%d" % (HOST, PORT)
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
