1246b9c9f81158e869fc715d98a7f7c9a1e06b48
zhangcheng <zhangcheng@zhangchengdeMacBook-Pro.local>
2014-04-01 11:17:36 +0800

    format

--- scrapy_top/scrapy_top.py
+++ scrapy_top/scrapy_top.py
@@ -10,40 +10,39 @@ HOST = 'localhost'
 PORT = 6023
 INTERVAL = 2
 
-opts, args = getopt.getopt(sys.argv[1:], "i:h:p:")
-
-for op, value in opts:
-    if op == "-i":
-        INTERVAL = int(value)
-    elif op == "-h":
-        HOST = value
-    elif op == "-p":
-        PORT == int(value)
 
 def main():
-    i = os.system("clear")
+    opts, args = getopt.getopt(sys.argv[1:], "i:h:p:")
+
+    for op, value in opts:
+        if op == "-i":
+            INTERVAL = int(value)
+        elif op == "-h":
+            HOST = value
+        elif op == "-p":
+            i = os.system("clear")
     try:
         tn = telnetlib.Telnet()
         tn.open(HOST, PORT)
     except:
         print "Can't connect to %s:%d" % (HOST, PORT)
         sys.exit()
-        while True:
-            try:
-                tn = telnetlib.Telnet()
-                tn.open(HOST, PORT)
-                tn.write('stats.get_stats()' + '\r\n')
-                ret = tn.read_until('}').splitlines()[3]
-                data = eval(ret)
-                x = PrettyTable(['Key', 'Value'])
-                for k, v in data.items():
-                    x.add_row([k, v])
-                    print x
-                    time.sleep(2)
-                    i = os.system("clear")
-            except:
-                print "Can't connect to %s:%d" % (HOST, PORT)
-                sys.exit()
+    while True:
+        try:
+            tn = telnetlib.Telnet()
+            tn.open(HOST, PORT)
+            tn.write('stats.get_stats()' + '\r\n')
+            ret = tn.read_until('}').splitlines()[3]
+            data = eval(ret)
+            x = PrettyTable(['Key', 'Value'])
+            for k, v in data.items():
+                x.add_row([k, v])
+            print x
+            time.sleep(2)
+            i = os.system("clear")
+        except:
+            print "Can't connect to %s:%d" % (HOST, PORT)
+            sys.exit()
 
 if __name__ == "__main__":
     main()
